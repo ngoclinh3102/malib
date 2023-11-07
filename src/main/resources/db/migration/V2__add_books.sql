77fed53cb1edc4b716c43b2973d21807d06466f9
ngoclinh3102 <linh.vu@logixtek.com>
2023-11-07 11:00:42 +0700

    add books

---
+++ src/main/resources/db/migration/V2__add_books.sql
@@ -0,0 +1,3 @@
+INSERT INTO book VALUES (5, 'ISBN1018', 'Pride and Prejudice', 'Jane Austen', 'NXB Kim Đồng', 165);
+INSERT INTO book VALUES (6, 'ISBN1019', 'Harry Potter and the Sorcerer''s Stone', 'J.K. Rowling', 'NXB Kim Đồng', 165);
+INSERT INTO book VALUES (7, 'ISBN1020', 'The Da Vinci Code', 'Dan Brown', 'NXB Kim Đồng', 165);
